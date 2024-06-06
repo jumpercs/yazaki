02a408f3eceabf5cae9e25ca532b47445fabd8fa
dev-abctechnology <60657808+jumpercs@users.noreply.github.com>
2024-06-06 13:51:37 -0300

    Refactor frame processing to improve performance and remove unnecessary print statement

--- Dados TI/index.py
+++ Dados TI/index.py
@@ -4,11 +4,11 @@ import cv2
 import os
 import time
 import json
-import threading
 
 # Caminho para a pasta 'Fotos'
 pasta_fotos = 'Fotos'
 
+founded_user = ""
 # Conexão com o banco de dados
 conn = sqlite3.connect('Dados_ti Banco Pronto.db')
 cursor = conn.cursor()
@@ -27,6 +27,7 @@ except FileNotFoundError:
     known_face_names = []
     cursor.execute("SELECT id_rede, nome FROM dados_ti")
     for row in cursor:
+        print(f"Carregando imagem para {row[0]}: {row[1]}")
         id_rede, nome = row
         try:
             imagem_banco = cv2.imread(os.path.join(pasta_fotos, f"{id_rede}.jpg"))
@@ -45,25 +46,19 @@ except FileNotFoundError:
 camera = cv2.VideoCapture(0)
 
 # Tamanho do frame buffer
-frame_buffer_size = 10
+frame_buffer_size = 1
 frame_buffer = []
 
 # Tempo de espera para a próxima verificação
-tempo_espera = 1  # Tempo em segundos
+tempo_espera = 1
 
 # Variável para controlar a verificação
-ultimo_reconhecimento = 0
-
-# Lock para sincronizar o acesso ao buffer
-lock = threading.Lock()
-
-# Variáveis para cálculo do frame rate
-frame_count = 0
-start_time = time.time()
+ultimo_reconhecimento = time.time()
 
 # Função para processar um frame
-def process_frame(frame, tempo_espera):  # Adicione tempo_espera como argumento
+def process_frame(frame):
     global ultimo_reconhecimento
+    global founded_user
     # Encontrar rostos no frame
     rostos_usuario = face_recognition.face_locations(frame)
 
@@ -74,74 +69,66 @@ def process_frame(frame, tempo_espera):  # Adicione tempo_espera como argumento
     if len(rostos_usuario) == 0:
         return frame
 
-    # Verificar se a verificação pode ser realizada
-    if time.time() - ultimo_reconhecimento >= tempo_espera:
-        # Loop para cada rosto detectado
-        for rosto_usuario, codificacao_usuario in zip(rostos_usuario, codificacoes_usuario):
+    # Loop para cada rosto detectado
+    for rosto_usuario, codificacao_usuario in zip(rostos_usuario, codificacoes_usuario):
+        print(codificacao_usuario)
 
-            # Comparar com as codificações faciais conhecidas
-            resultados_comparacao = face_recognition.compare_faces(known_face_encodings, codificacao_usuario, tolerance=0.15)
+        # Comparar com as codificações faciais conhecidas
+        resultados_comparacao = face_recognition.compare_faces(known_face_encodings, codificacao_usuario, tolerance=0.6)
 
-            # Verificar se o rosto é reconhecido
-            if True in resultados_comparacao:
+        # Verificar se o rosto é reconhecido
+        if True in resultados_comparacao:
+            # Verificar se a verificação pode ser realizada
+            if time.time() - ultimo_reconhecimento >= tempo_espera:
                 indice_correspondente = resultados_comparacao.index(True)
                 nome_usuario = known_face_names[indice_correspondente]
                 print(f"Usuário encontrado: {nome_usuario}")
-                cv2.putText(frame, f"Usuário: {nome_usuario}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                print(f"Porcentagem de similaridade: {face_recognition.face_distance([known_face_encodings[indice_correspondente]], codificacao_usuario)[0] * 100:.2f}%")
+  
+                founded_user = nome_usuario
                 ultimo_reconhecimento = time.time()  # Atualiza o último tempo de reconhecimento
-                break  # Sai do loop após encontrar um rosto reconhecido
         else:
             print("Usuário não reconhecido.")
-            cv2.putText(frame, "Usuário não reconhecido", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    # Calcular e exibir o frame rate
-    global frame_count, start_time
-    frame_count += 1
-    if time.time() - start_time >= 1:
-        fps = frame_count / (time.time() - start_time)
-        start_time = time.time()
-        frame_count = 0
-        cv2.putText(frame, f"FPS: {fps:.2f}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            founded_user = ""
+            ultimo_reconhecimento = time.time()  # Atualiza o último tempo de reconhecimento
 
     return frame
 
-# Função para atualizar a tela
-def atualizar_tela():
-    global frame_processado
-    while True:
-        with lock:
-            if frame_processado is not None:
-                cv2.imshow('Câmera', frame_processado)
-                frame_processado = None  # Limpa a variável após exibir o frame
 
-# Criar a thread para atualizar a interface
-thread_tela = threading.Thread(target=atualizar_tela)
-thread_tela.start()
+
+
+def process_frame_only_label(frame):
+    cv2.putText(frame, f"Usuário: {founded_user}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    return frame
+
 
 # Loop principal
 while(True):
     # Ler um frame da câmera
-    ret, frame = camera.read()
-
+    ret, frame = camera.read( )
+    # mirror
+   
     # Adicionar o frame ao buffer
-    with lock:  # Adquire o lock antes de acessar o buffer
-        frame_buffer.append(frame)
-        if len(frame_buffer) > frame_buffer_size:
-            frame_buffer.pop(0)
+    frame_buffer.append(frame)
+    if len(frame_buffer) > frame_buffer_size:
+        frame_buffer.pop(0)
+   
+    if time.time() - ultimo_reconhecimento < tempo_espera + 1:
+        
+        
+        frame_processado = process_frame_only_label(frame_buffer[-1])
+    else:
 
-    # Processar o frame mais antigo do buffer em uma thread separada
-    thread = threading.Thread(target=process_frame, args=(frame_buffer[0], tempo_espera))  # Passe tempo_espera
-    thread.start()
+        # Processar o frame mais antigo do buffer
+        frame_processado = process_frame(frame_buffer[0])
 
-    # Exibir o frame processado (pode não ser o mais recente)
-    # with lock:  # Adquire o lock para acessar o buffer com segurança
-    #     if len(frame_buffer) > 0:
-    #         frame_processado = frame_buffer[0]
-    #         cv2.imshow('Câmera', frame_processado)
+    # Exibir o frame processado
+    cv2.imshow('Câmera', frame_processado)
 
     # Pressione 'q' para sair
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 # Fechar a conexão com o banco de dados
 conn.close()
